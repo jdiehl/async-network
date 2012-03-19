c4b0a2c4262bada1cdf8acd7ffe8adcbce5a9c49
Jonathan Diehl <jdiehl@adobe.com>
2012-03-19 16:28:33 -0700

    updated the Headers

--- AsyncNetwork/AsyncNetworkFunctions.h
+++ AsyncNetwork/AsyncNetworkFunctions.h
@@ -1,10 +1,26 @@
-//
-//  AsyncNetworkFunctions.h
-//  ExampleBroadcaster
-//
-//  Created by Jonathan Diehl on 21.07.11.
-//  Copyright 2011 RWTH. All rights reserved.
-//
+/**
+ * Copyright (C) 2011 Jonathan Diehl
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * https://github.com/jdiehl/async-network
+ */
 
 #import <Foundation/Foundation.h>
 
@@ -12,11 +28,11 @@
  @brief Return the IP addresses of all local interfaces.
  @return Array of all local IP address strings
  */
-NSArray *AsyncNetworkGetLocalIPAddresses(void);
+extern NSArray *AsyncNetworkGetLocalIPAddresses(void);
 
 /**
  @brief Test if a given IP address string is local
  @param address The IP address string to be tested
  @return YES if the IP address string is local
  */
-BOOL AsyncNetworkIPAddressIsLocal(NSString *address);
+extern BOOL AsyncNetworkIPAddressIsLocal(NSString *address);
